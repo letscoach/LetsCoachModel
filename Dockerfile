df301e213863127ad508879e4d6695815b55843f
amichay turjeman <amichaytu@gmail.com>
2025-03-09 15:01:36 +0200

    cloud integration

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+
+FROM python:3.10
+
+ENV PYTHONUNBUFFERED True
+
+
+WORKDIR /app
+
+
+COPY . /app
+
+
+RUN pip install --no-cache-dir -r requirements.txt
+
+
+ENV PORT 8080
+
+CMD ["python", "main.py"]
--- main.py
+++ main.py
@@ -1,15 +1,19 @@
+import os
 import functions_framework
-from flask import Flask, request, jsonify
 from Game.Matches import game_launcher
 from Helpers.telegram_manager import send_log_message
+from flask import Flask, jsonify, request
+
 app = Flask(__name__)
 
-@functions_framework.http
-def LetscoachModel(request):
+
+@app.route("/", methods=["POST"])
+def LetscoachModel():
     """Cloud Function that runs the match algorithm"""
     try:
         send_log_message("Cloud function running now")
         request_json = request.get_json(silent=True)
+
         away_team_id = request_json.get('away_team_id')
         home_team_id = request_json.get('home_team_id')
         match_id = request_json.get('match_id')
@@ -20,5 +24,7 @@ def LetscoachModel(request):
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)  # הפעלה על פורט 8080
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
