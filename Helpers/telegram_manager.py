2e571ea62b5162432c16566fe67ebdb40718634e
amichay turjeman <amichaytu@gmail.com>
2025-03-10 23:13:21 +0200

    Scheduler

--- Helpers/telegram_manager.py
+++ Helpers/telegram_manager.py
@@ -7,8 +7,8 @@ def send_log_message(message):
 
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
     params = {"chat_id": GROUP_ID, "text": message}
-    # response = requests.get(url, params=params)
-    # print(response.json())
+    response = requests.get(url, params=params)
+    print(response.json())
 
 # res = requests.post('https://us-central1-zinc-strategy-446518-s7.cloudfunctions.net/LetscoachModel', json=dict(away_team_id=68,home_team_id=67,match_id=6), timeout=1000000)
 # print(res)
