6caf6908fe7521e197bee14f3bae4232e9a7009a
amichay turjeman <amichaytu@gmail.com>
2025-03-18 21:07:26 +0200

    training

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from Game.Matches import game_launcher, generate_schedule_double_round
 from Helpers.telegram_manager import send_log_message
 from flask import Flask, jsonify, request
 
+from Training.training import complete_training
+
 app = Flask(__name__)
 
 
@@ -14,11 +16,14 @@ def LetscoachModel():
         send_log_message("Cloud function running now")
         # request_json = dict(match_id=259,away_team_id=67,home_team_id=74)
         request_json = request.get_json(silent=True)
-        away_team_id = request_json.get('away_team_id')
-        home_team_id = request_json.get('home_team_id')
-        match_id = request_json.get('match_id')
+        if request_json.get('type') == 'training':
+            res = complete_training(request_json.get('training_id'))
+        else:
+            away_team_id = request_json.get('away_team_id')
+            home_team_id = request_json.get('home_team_id')
+            match_id = request_json.get('match_id')
 
-        res = game_launcher(dict(away_team_id=away_team_id, home_team_id=home_team_id, match_id=match_id))
+            res = game_launcher(dict(away_team_id=away_team_id, home_team_id=home_team_id, match_id=match_id))
 
         return jsonify({"message": res}), 200
     except Exception as e:
